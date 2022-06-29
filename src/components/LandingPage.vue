aa52c5bdf5f7db7284025a76b2898f78bd112c4f
Evans Allison <allisonevans081@gmail.com>
2022-06-29 14:46:20 +0000

    Adding the paragraph element

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="landing__page" class="h-screen w-screen bg-blue-400 flex flex-row items-center justify-center">
-        <div id="landing__text-section" class="bg-red-400 h-full w-1/2 flex pl-16 items-center ">
+        <div id="landing__text-section" class="bg-red-400 h-full w-1/2 flex flex-col pl-16 items-start justify-center">
             <img src="../assets/images/headline.png" class="h-80">
+            <p class="w-96 m-4">Adipiscing commodo, eget morbi sagittis. Quis dignissim viverra ultrices enim, massa dolor dolor eget. Ultrices ornare quam tristique pharetra. Vel, sit scelerisque commodo libero,</p>
         </div>
 
         <div id="landing__image-section" class="bg-green-400 h-full w-1/2">
