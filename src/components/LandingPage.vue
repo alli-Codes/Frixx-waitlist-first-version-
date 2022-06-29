36ba0b88f16514cbaf1dcef2c2a0ced228af3d3b
Evans Allison <allisonevans081@gmail.com>
2022-06-29 12:58:21 +0000

    Creating the Landing-Page section

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="landing__page" class="h-screen bg-blue-400">
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'LandingPage',
+        
+    }
+
+</script>
