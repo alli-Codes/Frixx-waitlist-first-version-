b3f844449b7d536d90e60993cbe3b4035deb1918
Evans Allison <allisonevans081@gmail.com>
2022-06-29 14:23:51 +0000

    Adding the headline as an image file

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,6 +1,12 @@
 <template>
-    <div id="landing__page" class="h-screen bg-blue-400">
+    <div id="landing__page" class="h-screen w-screen bg-blue-400 flex flex-row items-center justify-center">
+        <div id="landing__text-section" class="bg-red-400 h-full w-1/2 flex pl-16 items-center ">
+            <img src="../assets/images/headline.png" class="h-80">
+        </div>
 
+        <div id="landing__image-section" class="bg-green-400 h-full w-1/2">
+        
+        </div>
     </div>
 </template>
 
@@ -8,7 +14,7 @@
 
     export default {
         name: 'LandingPage',
-        
+
     }
 
 </script>
