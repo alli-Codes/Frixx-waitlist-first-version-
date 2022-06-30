c88bfbc9f8227f4f106000954eb1bed0d74f0990
Evans Allison <allisonevans081@gmail.com>
2022-06-30 14:09:15 +0000

    adding the Logo Component

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,5 +1,18 @@
 <template>
     <div id="header" class="w-full h-16 bg-blue-400 fixed top-0">
-
+        <Icon />
     </div>
 </template>
+
+<script>
+
+    import Icon from './Icon.vue'
+    
+    export default {
+        name: 'Header',
+        components: {
+            Icon
+        }
+    }
+
+</script>
