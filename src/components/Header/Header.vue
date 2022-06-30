1a5b68ac432a0434bb962543100930a9f3a2f983
Evans Allison <allisonevans081@gmail.com>
2022-06-30 14:35:03 +0000

    Adding the theme mode icon in the assets folder

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,7 @@
 <template>
-    <div id="header" class="w-full h-16 bg-blue-400 fixed top-0">
+    <div id="header" class="bg-slate-50 w-full h-16 px-32 flex justify-between items-center fixed top-0">
         <Icon />
+        <img src="../../assets/images/mode.png" class="w-4">
     </div>
 </template>
 
