299a176999c9bc416ca30bbb8ee91df2b31dabfd
Evans Allison <allisonevans081@gmail.com>
2022-07-01 07:25:11 +0000

    Adjusting the background image

--- src/components/Connect.vue
+++ src/components/Connect.vue
@@ -1,6 +1,6 @@
 <template>
-    <div id="container" class="h-screen p-4 flex items-center justify-center">
-        <div id="connect__page" class="bg-connect-pattern bg-no-repeat bg-center h-full w-full">
+    <div id="container" class="h-screen p-32 flex items-center justify-center">
+        <div id="connect__page" class="bg-connect-pattern bg-no-repeat bg-contain bg-center h-full w-full">
         
         </div>
     </div>
