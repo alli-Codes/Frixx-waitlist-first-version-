85562a8e43bdbf25ce7a38782899f3679594889b
Evans Allison <allisonevans081@gmail.com>
2022-07-01 12:43:44 +0000

    Creating a subdirectory called setiions to store the Section conmponents, also importing them into the FeaturePage

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <Header />
   <LandingPage />
   <ConnectPage />
+  <FeaturePage />
 </template>
 
 <script>
@@ -9,13 +10,15 @@
   import Header from './components/Header/Header.vue'
   import LandingPage from './components/landing-page/LandingPage.vue'
   import ConnectPage from './components/ConnectPage.vue'
+  import FeaturePage from './components/Features/FeaturePage.vue'
 
   export default {
     name: 'App',
     components: {
       Header,
       LandingPage,
-      ConnectPage
+      ConnectPage,
+      FeaturePage
     }
   }
 
--- src/components/Features/FeaturePage.vue
+++ src/components/Features/FeaturePage.vue
@@ -1,5 +1,28 @@
 <template>
     <div id="container">
-        
+        <Section1 /> 
+        <Section2 /> 
+        <Section3 /> 
+        <Section4 /> 
     </div>
 </template>
+
+<script>
+
+    import Section1 from './sections/Section1.vue'
+    import Section2 from './sections/Section2.vue'
+    import Section3 from './sections/Section3.vue'
+    import Section4 from './sections/Section4.vue'
+
+    export default {
+        name: 'FeaturePage',
+        components: {
+            Section1,
+            Section2,
+            Section3,
+            Section4,
+        }
+    }
+
+</script>
+
