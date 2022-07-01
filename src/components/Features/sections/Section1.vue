05994ec2f99fd42b57863c63865f9f2c2b80dffd
Evans Allison <allisonevans081@gmail.com>
2022-07-01 13:16:27 +0000

    Adding the images for the FeaturePage component

--- src/components/Features/sections/Section1.vue
+++ src/components/Features/sections/Section1.vue
@@ -1,3 +1,17 @@
 <template>
+
+    <h1>Some of Our Features</h1>
+
+    <div class=" flex">
+        <div>
+            <img src="../../../assets/feature-1.png">
+        </div>
+
+        <div>
+            <h1>Live Chatrooms</h1>
+            <p>Imperdiet sit eu sed tortor egestas id. A consectetur ac fringilla nisi. Vitae et sed ante dictum diam scelerisque vulputate at pretium. Sit porttitor ac tortor pretium pellentesque. Duis fermentum.</p>
+            <button>Join the waitlist</button>
+        </div>
+    </div>
     
 </template>
