ddccce055f042ee043a141802dc5e3fb8163238f
alli-Codes <allisonevans081@gmail.com>
2022-07-14 11:49:58 +0100

    Styling the Header component

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="header" class="bg-slate-50 w-full h-16 px-32 flex justify-between items-center fixed top-0 z-[9999]">
+    <div id="header" class="bg-slate-50 w-full h-16 px-4 lg:px-32 flex justify-between items-center fixed top-0 z-[9999]">
         <Icon />
         <div class="cursor-pointer">
             <img src="/images/mode.png" class="w-4">
