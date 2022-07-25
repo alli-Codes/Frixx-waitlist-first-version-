052dfd77287466b4ea7a7a712e5e34191a362790
alli-Codes <allisonevans081@gmail.com>
2022-07-25 17:06:03 +0100

    Installing a package called swiper responsible for sliding effect and implementing it in the app and also solving some f*cking bugs

---
+++ src/components/Faq/Faq.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="h-full flex flex-col space-y-20">
+        <div class="flex items-center justify-center space-y-8">
+            <h1 class="font-bold relative z-[100] text-xl lg:text-xl font-gorgagrotesque-regular lg:w-3/6 text-center ">Imperdiet sit eu
+                sed tortor
+                egestas id. A
+                consectetur ac fringilla
+                nisi.</h1>
+            <Circle v-show="false" :style="styles" class="w-80 lg:w-auto h-36 lg:h-auto" />
+        </div>
+
+        <div class="flex flex-row justify-center items-center gap-4">
+                <img :src="person.image" class="w-12 h-12" alt="xavier photo's">
+                <div class="flex flex-col space-y-2">
+                    <p class="font-bold">{{person.name}}</p>
+                    <p class="text-sm">{{person.title}}</p>
+                </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Circle from '../landing-page/Circle.vue';
+
+    export default {
+        name: 'Faq',
+        components: {
+            Circle
+        },
+        data() {
+            return {
+                styles: {
+                    strokeColor: '#8E25B6',
+                    top: '60',
+                    left: '',
+                }
+            }
+        },
+        props: {
+            person: Object,
+            slider: Function
+        },
+    }
+
+</script>
+
--- src/components/Faq/FaqPage.vue
+++ src/components/Faq/FaqPage.vue
@@ -1,39 +1,80 @@
 <template>
-    <div id="faq" class="grid place-items-center py-5 px-0 lg:px-32 relative">
-        <div>
-            <img src="/images/quote.svg" alt="">
-        </div>
-        <div class="h-64 flex items-center justify-center space-y-8">
-            <h1 class="font-bold relative z-[100] text-xl lg:text-4xl font-gorgagrotesque-regular lg:w-3/6 text-center ">Imperdiet sit eu
-                sed tortor
-                egestas id. A
-                consectetur ac fringilla
-                nisi.</h1>
-            <Circle v-show="false" :style="styles" class="w-80 lg:w-auto h-36 lg:h-auto" />
-        </div>
-        <div class="flex flex-row justify-center items-center gap-4">
-            <img src="/images/xavier.png" class="w-12 h-12" alt="xavier photo's">
-            <div class="flex flex-col space-y-2">
-                <p class="font-bold">Uwakmfon inemesit</p>
-                <p>Senior Product Designer</p>
-            </div>
-        </div>
-        <div class="mt-6">
-            <img src="/images/bars.svg" class="w-12 h-12" alt="">
-        </div>
-    </div>
-</template>
-
-
-<script setup>
-import Circle from '../landing-page/Circle.vue';
-    import { ref } from 'vue';
-
+  <swiper
+    :slidesPerView="1"
+    :spaceBetween="30"
+    :loop="true"
+    :autoplay="{
+      delay: 3500,
+      disableOnInteraction: false,
+    }"
+    :pagination="{
+      clickable: true,
+    }"
+    :modules="modules"
     
+    class="h-96 w-full lg:w-3/4 p-8 flex flex-col-reverse items-center"
+  >
+    <div>
+        <img src="/images/quote.svg" alt="" class="h-16 w-8">
+    </div>
+    <swiper-slide v-for="person in person" class="h-full flex items-center justify-center flex-col"><Faq :person="person" /></swiper-slide>
 
-const styles = ref({
-    strokeColor: '#8E25B6',
-    top: '60',
-    left: '',
-})
+  </swiper>
+</template>
+<script>
+import Faq from './Faq.vue'
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./style.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+export default {
+    name: 'test',
+  components: {
+    Faq,
+    Swiper,
+    SwiperSlide,
+  }, 
+  data(){
+            return{
+                person: [
+                    {
+                        image: '/images/evans.png',
+                        name: 'Tobi Emeka',
+                        title: 'Frontend Dev/ CEWhore',
+                        quote: 'Imperdiet sit eused tortor egestas id. Aconsectetur ac fringillanisi.'
+                    },
+
+                    {
+                        image: '/images/evans.png',
+                        name: 'Evans Allison',
+                        title: 'Frontend Dev/ CTO',
+                        quote: 'Imperdiet sit eused tortor egestas id. Aconsectetur ac fringillanisi.'
+                    },
+
+                    {
+                        image: '/images/evans.png',
+                        name: 'No name',
+                        title: 'Media Lead/ CMO',
+                        quote: 'Imperdiet sit eused tortor egestas id. Aconsectetur ac fringillanisi.'
+                    },
+                ],
+                index: 0,
+            }
+  },
+  setup() {
+    return {
+      modules: [Autoplay, Pagination, Navigation],
+    };
+  },
+};
 </script>
